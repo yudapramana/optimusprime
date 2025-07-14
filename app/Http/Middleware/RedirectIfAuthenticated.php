6221251ae3953a4786ebbe9ceaf0c1b76ef619c7
pramana yuda <pramanayuda772@gmail.com>
2025-07-14 19:33:47 +0700

    push

--- app/Http/Middleware/RedirectIfAuthenticated.php
+++ app/Http/Middleware/RedirectIfAuthenticated.php
@@ -21,7 +21,12 @@ class RedirectIfAuthenticated
 
         foreach ($guards as $guard) {
             if (Auth::guard($guard)->check()) {
-                return redirect(RouteServiceProvider::HOME);
+                // return redirect(RouteServiceProvider::HOME);
+                if (auth()->user()->role == 'admin') {
+                    return redirect()->route('admin.home');
+                } else{
+                    return redirect()->route('student.home');
+                }
             }
         }
 
